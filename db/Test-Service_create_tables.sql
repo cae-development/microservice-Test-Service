7b1fa474823f97f56aa3fa0d429785384ce3cf1b
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-06-26 14:33:02 +0200

    undefined edited by Thomas Winkler

--- db/Test-Service_create_tables.sql
+++ db/Test-Service_create_tables.sql
@@ -7,7 +7,7 @@
 -- Table structure for table test.
 --
 CREATE TABLE test.test (
-  id int NOT NULL AUTO_INCREMENT,
+  id int NOT NULL AUTO_INCREMENT ,
   number int ,
 CONSTRAINT id_PK PRIMARY KEY (id)
 );
