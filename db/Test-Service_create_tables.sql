5b79cffdc4cc1afdef029aff1d80d10aa0ce82d7
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-06-26 13:01:27 +0200

    Generated microservice version 0.1

---
+++ db/Test-Service_create_tables.sql
@@ -0,0 +1,14 @@
+--
+-- Database Schema:  test 
+-- Automatically generated sql script for the service Test-Service, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table test.
+--
+CREATE TABLE test.test (
+  id int ,
+  number int ,
+CONSTRAINT id_PK PRIMARY KEY (id)
+);
+
