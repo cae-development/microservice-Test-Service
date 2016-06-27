b63fc51954fb8a86080541932a70cada136dca25
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-06-27 15:58:21 +0200

    Code regeneration/Model synchronization

---
+++ db/Test-Service_create_tables.sql
@@ -0,0 +1,13 @@
+--
+-- Database Schema:  test 
+-- Automatically generated sql script for the service Test-Service, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table test.
+--
+CREATE TABLE test.test (
+  test int ,
+CONSTRAINT test_PK PRIMARY KEY (test)
+);
+
