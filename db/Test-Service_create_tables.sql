5875ec60e73f346a5c8c7cb5d07e724be7fe58e5
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-06-27 22:04:09 +0200

    Code regeneration/Model synchronization

--- db/Test-Service_create_tables.sql
+++ db/Test-Service_create_tables.sql
@@ -4,6 +4,13 @@
 -- --------------------------------------------------------
 
 --
+-- Table structure for table test2.
+--
+CREATE TABLE test.test2 (
+  id int ,
+CONSTRAINT id_PK PRIMARY KEY (id)
+);
+--
 -- Table structure for table test.
 --
 CREATE TABLE test.test (
